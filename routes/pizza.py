ff67968fcf4b5115625a926a3a293d4eafe64ec2
Stealth298730 <serhii.lundiak@gmail.com>
2024-10-02 19:24:42 +0300

    first commit

---
+++ routes/pizza.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, render_template, request, redirect
+
+from models.base import Session
+from models.pizza import Pizza
+from models.ingredient import Ingredient
+from data.wheather import get_wheather
+
+
+pizza_route = Blueprint("pizzas", __name__)
+
+
+@pizza_route.get("/")
+def index():
+    wheather = get_wheather("Neratovice")
+    temp = wheather.get("temp")
+    pizza_name = "Невідома піцца"
+    if temp is not None:
+        if 26 > temp > 10:
+            pizza_name = "Тепла"
+        elif temp <= 10:
+            pizza_name = "Холодна"
+        elif temp > 26:
+            pizza_name = "Пепероні"
+
+
+    return render_template("index.html", title="Моя супер піцерія", wheather=wheather, pizza_name=pizza_name)
+
+
+@pizza_route.get("/menu/")
+def menu():
+    wheather = get_wheather("Kyiv")
+    with Session() as session:
+        pizzas = session.query(Pizza).all()
+        ingredients = session.query(Ingredient).all()
+
+        context = {
+            "pizzas": pizzas,
+            "ingredients": ingredients,
+            "title": "Мега меню",
+            "wheather": wheather
+        }
+        return render_template("menu.html", **context)
+
+
+@pizza_route.post("/add_pizza/")
+def add_pizza():
+    with Session() as session:
+        name = request.form.get("name")
+        price = request.form.get("price")
+        ingredients = request.form.getlist("ingredients")
+        ingredients = session.query(Ingredient).where(Ingredient.id.in_(ingredients)).all()
+
+        pizza = Pizza(name=name, price=price, ingredients=ingredients)
+        session.add(pizza)
+        session.commit()
+        return redirect("/menu/")
